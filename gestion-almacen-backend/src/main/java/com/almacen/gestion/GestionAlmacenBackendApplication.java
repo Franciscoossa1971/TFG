e0c584308f002e1bd4f59b10227795857381d164
francisco <franciscoossamarquez@gmail.com>
2025-03-29 18:10:33 +0100

    Versión inicial backend

---
+++ gestion-almacen-backend/src/main/java/com/almacen/gestion/GestionAlmacenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.almacen.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAlmacenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAlmacenBackendApplication.class, args);
+	}
+
+}
---
+++ gestion-almacen-backend/src/test/java/com/almacen/gestion/GestionAlmacenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almacen.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAlmacenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
