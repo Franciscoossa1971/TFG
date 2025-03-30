986d1e4b276d89a290c3c61bba2c866903811949
francisco <franciscoossamarquez@gmail.com>
2025-03-30 13:12:46 +0200

    Header y footer

--- gestion-almacen-frontend/src/app/app.component.ts
+++ gestion-almacen-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './shared/components/footer/footer.component'; 
+import { HeaderComponent } from './shared/components/header/header.component'; 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gestion-almacen-frontend';
